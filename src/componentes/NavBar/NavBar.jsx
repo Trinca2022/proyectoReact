3d45c2e88f12a4b5017ff167a1a77d01059c704e
Agustin Trinca <catrincavelli@gmail.com>
2023-01-22 20:47:43 -0300

    final entrega 1

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,19 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from '../CartWidget/CartWidget';
 
 const NavBar = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#"><img className="w-25" src="./Imagenes/Logo Don Julio.jpg" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="#features">Café</Nav.Link>
+                        <Nav.Link href="#pricing">Cafeteras</Nav.Link>
+                        <Nav.Link href="#pricing">Vajilla</Nav.Link>
+                        <Nav.Link href="#features">Accesorios</Nav.Link>
+
 
                     </Nav>
                     <Nav>
