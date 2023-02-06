d600d6dec43318a6003f292df5845cd58248309e
Agustin Trinca <catrincavelli@gmail.com>
2023-02-06 11:34:10 -0300

    agrego itemDetailContainer funcionando

--- src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/componentes/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { gFetch } from '../../utils/gFetch'
 
 export const ItemDetailContainer = () => {
+
+    const [productos, setProductos] = useState([])
+
     const { idProducto } = useParams()
+
     console.log(idProducto)
+    useEffect(() => {
+        gFetch()
+            .then(res => {
+                setProductos(res.filter(producto => producto.id === idProducto))
+            })
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+
+    }, [])
 
     return (
-        <div>ItemDetailContainer</div>
+        <>
+
+            <div>
+                {productos.map(producto => <div key={producto.id}>{producto.nombre}</div>)}
+            </div>
+        </>
     )
 }
--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,5 +1,4 @@
 import { Link, NavLink } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
