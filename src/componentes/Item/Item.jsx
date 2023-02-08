7bfe919721a84d22a5db950e0d3d5afc84930ee8
Agustin Trinca <catrincavelli@gmail.com>
2023-02-08 00:48:15 -0300

    agrego detalles

--- src/componentes/Item/Item.jsx
+++ src/componentes/Item/Item.jsx
@@ -5,13 +5,13 @@ const Item = ({ producto }) => {
     return (
         <div className='card w-40 mt-2' >
             <div className='card-header'>
-                Nombre: {producto.nombre}
+                {producto.nombre}
             </div>
             <div className='card-body'>
                 <img src={producto.foto} alt='foto' className="w-100"></img>
             </div>
             <div className='card-footer'>
-                Precio: {producto.precio}<br />
+                Precio: ${producto.precio}<br />
                 <Link to={`/ItemDetail/${producto.id}`}>
                     <button className="btn btn-outline-primary w-100">Ver producto</button>
                 </Link>
--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const ItemDetail = ({ producto }) => {
     return (
-        <div>
+        <div className='card w-40 mt-2' >
             <div className='card-header'>
-                Nombre: {producto.nombre}
+                {producto.nombre}
             </div>
             <div className='card-body'>
                 <img src={producto.foto} alt='foto' className="w-100" />
             </div>
             <div className='card-footer'>
-                Precio: {producto.precio}
+                Precio: ${producto.precio}
             </div>
             <button className="btn btn-outline-primary w-100">Añadir al carrito</button>
         </div>
