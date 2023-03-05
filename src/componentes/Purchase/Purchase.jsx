b2263f8f82bdae80553b78fc3a7dbad78d7ea2dd
Agustin Trinca <catrincavelli@gmail.com>
2023-03-05 19:18:34 -0300

    agrego estilos

--- src/componentes/Purchase/Purchase.jsx
+++ src/componentes/Purchase/Purchase.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
-import Loading from "../Loading/Loading"
 
 const Purchase = () => {
     const { idOrder } = useParams()
